e1dcea7f2a727d68e1a8aaab3a8b77a0194a52f4
Suyesha Chaudhary <suyeshaachaudhry@gmail.com>
2021-07-06 23:30:46 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,71 @@
+var num = "";
+var newNum = "";
+
+var op = [];
+
+$("button").click((e) => {
+  var curr = e.target.innerText;
+  console.log(e.target.innerText);
+  if (isNaN(curr) == false) {
+    num += curr;
+    $(".inp-box").text(num);
+  }
+
+  if (curr == ".") {
+    num += curr;
+  }
+
+  if (curr == "+") {
+    op.push(num);
+    op.push("+");
+    num = "";
+    console.log(op);
+  }
+  if (curr == "-") {
+    op.push(num);
+    op.push("-");
+    num = "";
+    console.log(op);
+  }
+  if (curr == "/") {
+    op.push(num);
+    op.push("/");
+    num = "";
+    console.log(op);
+  }
+  if (curr == "X") {
+    op.push(num);
+    op.push("*");
+    num = "";
+    console.log(op);
+  }
+
+  if (curr == "=") {
+    op.push(num);
+    // console.log(eval(op[0] + op[1] + op[2]));
+    operations();
+    num = "";
+    op = [];
+    newNum = "";
+  }
+
+  if (curr == "DEL") {
+    op.push(num);
+    op.pop();
+    console.log(op);
+  }
+});
+
+function operations() {
+  op.forEach((el) => {
+    newNum += el;
+    console.log(newNum);
+  });
+
+  console.log(eval(newNum));
+}
+
+// document.querySelector("button").addEventListener("click", (e) => {
+//   console.log();
+// });
+// console.log(document.querySelector("button").textContent);
