cb518e2cfaa2631667456387dbc8c40d73c9c01b
Suyesha Chaudhary <suyeshaachaudhry@gmail.com>
2021-07-09 20:26:38 +0530

    add logo and alter js

--- main2.js
+++ main2.js
@@ -4,31 +4,67 @@ var op = "";
 var newNum = "";
 
 function append(num) {
+  if (num === "." && curr.includes(".")) {
+    return;
+  }
   if (isNaN(num) == false || num === ".") {
     curr += num;
+    $(".inp-box").text(curr);
     console.log(curr);
   }
 }
 
-compute(){
-
+function compute(n) {
+  $(".inp-box").text("");
+  console.log(prev, n, curr);
+  console.log(eval(prev + n + curr));
+  curr = eval(prev + n + curr) + "";
+  $(".inp-box").text(curr);
+  prev = "";
 }
 
 function operation(num) {
-  if (num === "+" || num === "-" || num === "*" || num === "/") {
+  if (num === "+" || num === "-" || num === "*" || num === "/" || num === "=") {
+    $(".inp-box").text("");
     if (curr === "") {
       return;
     }
-    if (prev != "") {
-      compute();
+    if (prev != "" || num === "=") {
+      compute(oper);
     }
-    oper = num;
-    prev = curr;
-    curr = "";
+
+    if (num !== "=") {
+      oper = num;
+      prev = curr;
+      curr = "";
+    }
   }
 }
 
+function reset() {
+  prev = "";
+  curr = "";
+  op = "";
+  newNum = "";
+  $(".inp-box").text("");
+}
+
+function delte() {
+  curr = curr.substr(0, curr.length - 1);
+  $(".inp-box").text(curr);
+}
+
 $("button").click((e) => {
-  console.log(e.target.innerText);
-  append(e.target.innerText);
+  var txt = e.target.innerText;
+  console.log(txt);
+  append(txt);
+  operation(txt);
+
+  if (txt === "AC") {
+    reset();
+  }
+
+  if (txt === "C") {
+    delte();
+  }
 });
